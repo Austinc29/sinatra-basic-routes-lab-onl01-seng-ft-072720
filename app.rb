7fe69ce90342b91240b432d99103a9e5a1658573
Austin Cummings <aac729@live.com>
2020-09-07 21:07:23 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -5,4 +5,11 @@ class App < Sinatra::Base
   get '/name' do 
     "My name is Austin"
   end
+  
+  get '/hometown' do 
+    "My hometown is Joliet"
+  end 
+  
+  get '/favorite-song' do 
+    "My favorite song its Sad"
 end
